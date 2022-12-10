d424d7467d2d814b515af1c6267d9fe0614de1ee
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 13:57:49 -0800

    Update DarkChocolateTruffleActivity.java

--- app/src/main/java/com/example/coffeeappv1/DarkChocolateTruffleActivity.java
+++ app/src/main/java/com/example/coffeeappv1/DarkChocolateTruffleActivity.java
@@ -13,4 +13,29 @@ public class DarkChocolateTruffleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dark_chocolate_truffle);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+    
+    public void buttonPressBreakfastBlend(View view) {
+        Intent intent = new Intent(this, BreakfastBlendActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressWestsideBlend(View view) {
+        Intent intent = new Intent(this, WestsideBlendActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressJamaicaBlueMountainFancy(View view) {
+        Intent intent = new Intent(this, JamaicaBlueMountainFancyActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressOrganicFairTradeRendezvousBrew(View view) {
+        Intent intent = new Intent(this, OrganicFairTradeRendezvousBrewActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressEuropeanRoyale(View view) {
+        Intent intent = new Intent(this, EuropeanRoyaleActivity.class);
+        startActivity(intent);
+    }
 }
