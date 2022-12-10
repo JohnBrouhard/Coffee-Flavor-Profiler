1a36d2c70e9108ba7ff7f300355612dec91395c6
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 14:17:34 -0800

    Update DecaffeinatedVanillaNutCreamActivity.java

--- app/src/main/java/com/example/coffeeappv1/DecaffeinatedVanillaNutCreamActivity.java
+++ app/src/main/java/com/example/coffeeappv1/DecaffeinatedVanillaNutCreamActivity.java
@@ -13,4 +13,29 @@ public class DecaffeinatedVanillaNutCreamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_decaffeinated_vanilla_nut_cream);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+
+    public void buttonPressCinnamonNutSugarCookie(View view) {
+        Intent intent = new Intent(this, CinnamonNutSugarCookieActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressFrenchVanilla(View view) {
+        Intent intent = new Intent(this, FrenchVanillaActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressCaramelNutFudge(View view) {
+        Intent intent = new Intent(this, CaramelNutFudgeActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressBourbonPecan(View view) {
+        Intent intent = new Intent(this, BourbonPecanActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressVanillaMacadamiaNut(View view) {
+        Intent intent = new Intent(this, VanillaMacadamiaNutActivity.class);
+        startActivity(intent);
+    }
 }
