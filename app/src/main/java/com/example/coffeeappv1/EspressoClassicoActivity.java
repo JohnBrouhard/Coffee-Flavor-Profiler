afa097ff924ee2fc733c37934950813015d3b679
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 14:20:43 -0800

    Update EspressoClassicoActivity.java

--- app/src/main/java/com/example/coffeeappv1/EspressoClassicoActivity.java
+++ app/src/main/java/com/example/coffeeappv1/EspressoClassicoActivity.java
@@ -13,4 +13,29 @@ public class EspressoClassicoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_espresso_classico);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+
+    public void buttonPressVanillaMacadamiaNut(View view) {
+        Intent intent = new Intent(this, VanillaMacadamiaNutActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressVanillaNutCream(View view) {
+        Intent intent = new Intent(this, VanillaNutCreamActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressOrganicFairTradeRendezvousBrew(View view) {
+        Intent intent = new Intent(this, OrganicFairTradeRendezvousBrewActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressCostaRicaLaEstrella(View view) {
+        Intent intent = new Intent(this, CostaRicaLaEstrellaActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressBarbaryCoastBlend(View view) {
+        Intent intent = new Intent(this, BarbaryCoastBlendActivity.class);
+        startActivity(intent);
+    }
 }
