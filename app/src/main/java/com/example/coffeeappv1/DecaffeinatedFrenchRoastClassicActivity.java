350617003f849cd2222715686b6c60bdd99f81ce
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 14:10:21 -0800

    Update DecaffeinatedFrenchRoastClassicActivity.java

--- app/src/main/java/com/example/coffeeappv1/DecaffeinatedFrenchRoastClassicActivity.java
+++ app/src/main/java/com/example/coffeeappv1/DecaffeinatedFrenchRoastClassicActivity.java
@@ -13,4 +13,29 @@ public class DecaffeinatedFrenchRoastClassicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_decaffeinated_french_roast_classic);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+
+    public void buttonPressFrenchRoastClassic(View view) {
+        Intent intent = new Intent(this, FrenchRoastClassicActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressOrganicFairTradeMillenniumJoe(View view) {
+        Intent intent = new Intent(this, OrganicFairTradeMillenniumJoeActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressOrganicHawaiian(View view) {
+        Intent intent = new Intent(this, OrganicHawaiianActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressGuatemalaAntiguaDark(View view) {
+        Intent intent = new Intent(this, GuatemalaAntiguaDarkActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressDecaffeinatedColombia(View view) {
+        Intent intent = new Intent(this, DecaffeinatedColombiaActivity.class);
+        startActivity(intent);
+    }
 }
