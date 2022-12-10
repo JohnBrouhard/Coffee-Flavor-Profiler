bf9242ab4ecd72e138646b8d44a10c2cd82b537d
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 13:52:25 -0800

    Update CinnamonNutSugarCookieActivity.java

--- app/src/main/java/com/example/coffeeappv1/CinnamonNutSugarCookieActivity.java
+++ app/src/main/java/com/example/coffeeappv1/CinnamonNutSugarCookieActivity.java
@@ -13,4 +13,29 @@ public class CinnamonNutSugarCookieActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cinnamon_nut_sugar_cookie);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+
+    public void buttonPressVanillaNutCream(View view) {
+        Intent intent = new Intent(this, VanillaNutCreamActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressDecaffeinatedVanillaNutCream(View view) {
+        Intent intent = new Intent(this, DecaffeinatedVanillaNutCreamActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressCinnamonBun(View view) {
+        Intent intent = new Intent(this, CinnamonBunActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressFrenchVanilla(View view) {
+        Intent intent = new Intent(this, FrenchVanillaActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressCinnamonFrangelico(View view) {
+        Intent intent = new Intent(this, CinnamonFrangelicoActivity.class);
+        startActivity(intent);
+    }
 }
