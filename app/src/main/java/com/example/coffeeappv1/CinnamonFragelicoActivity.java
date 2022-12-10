98b9db35a2f0b886149dae664e3e412012953b13
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 13:50:34 -0800

    Update CinnamonFragelicoActivity.java

--- app/src/main/java/com/example/coffeeappv1/CinnamonFragelicoActivity.java
+++ app/src/main/java/com/example/coffeeappv1/CinnamonFragelicoActivity.java
@@ -13,4 +13,29 @@ public class CinnamonFragelicoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cinnamon_fragelico);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+    
+    public void buttonPressDecaffeinatedHazelnut(View view) {
+        Intent intent = new Intent(this, DecaffeinatedHazelnutActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressHazelnut(View view) {
+        Intent intent = new Intent(this, HazelnutActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressCaramelNutFudge(View view) {
+        Intent intent = new Intent(this, CaramelNutFudgeActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressCinnamonNutSugarCookie(View view) {
+        Intent intent = new Intent(this, CinnamonNutSugarCookieActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressCinnamonBun(View view) {
+        Intent intent = new Intent(this, CinnamonBunActivity.class);
+        startActivity(intent);
+    }
 }
