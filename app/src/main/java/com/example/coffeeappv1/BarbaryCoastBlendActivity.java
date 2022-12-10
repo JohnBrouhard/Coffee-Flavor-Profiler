3a85ad657e3e88aca95fabb3a203b3c7b1d74474
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 13:27:17 -0800

    Update BarbaryCoastBlendActivity.java

--- app/src/main/java/com/example/coffeeappv1/BarbaryCoastBlendActivity.java
+++ app/src/main/java/com/example/coffeeappv1/BarbaryCoastBlendActivity.java
@@ -13,4 +13,30 @@ public class BarbaryCoastBlendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_barbary_coast_blend);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+    
+    public void buttonPressViennaDark(View view) {
+        Intent intent = new Intent(this, ViennaDarkActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressRivieraFrench(View view) {
+        Intent intent = new Intent(this, RivieraFrenchActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressMidnightRoyale(View view) {
+        Intent intent = new Intent(this, MidnightRoyaleActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressFrenchRoastDarkItalian(View view) {
+        Intent intent = new Intent(this, FrenchRoastDarkItalianRoastActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressEspressoNuovo(View view) {
+        Intent intent = new Intent(this, EspressoNuovoActivity.class);
+        startActivity(intent);
+    }
+    
 }
